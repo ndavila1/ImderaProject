514c29dc8d02762c105ac2d1911070dd17545685
Nicolas Davila <nikojako99@gmail.com>
2019-09-14 16:03:58 -0500

    Implememtacion del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventosComponent } from './componentes/herramientas/eventos/eventos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'eventos',component: EventosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ActividadFisicaModule } from './componentes/actividad-fisica/actividad-fisica.module';
+import { ApoyoOrganizacionesModule } from './componentes/apoyo-organizaciones/apoyo-organizaciones.module';
+import { DeporteEscolarModule } from './componentes/deporte-escolar/deporte-escolar.module';
+import { DeporteSocialModule } from './componentes/deporte-social/deporte-social.module';
+import { EstaticasModule } from './componentes/estaticas/estaticas.module';
+import { MantenimientoModule } from './componentes/mantenimiento/mantenimiento.module';
+import { ParametricasModule } from './componentes/parametricas/parametricas.module';
+import { RecreandoComunidadModule } from './componentes/recreando-comunidad/recreando-comunidad.module';
+import { HerramientasModule } from './componentes/herramientas/herramientas.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ActividadFisicaModule,
+    ApoyoOrganizacionesModule,
+    DeporteEscolarModule,
+    DeporteSocialModule,
+    EstaticasModule,
+    MantenimientoModule,
+    ParametricasModule,
+    RecreandoComunidadModule,
+    HerramientasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
