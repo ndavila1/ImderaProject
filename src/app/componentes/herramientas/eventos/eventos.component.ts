c38ba4c2647357d980cde870efabdddc4df4de98
Nicolas Davila <nikojako99@gmail.com>
2019-09-14 18:33:45 -0500

    Modal eventos creado

--- src/app/componentes/herramientas/eventos/eventos.component.ts
+++ src/app/componentes/herramientas/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-eventos',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  createFormGroup() {
+    return new FormGroup({
+      lugar: new FormControl('', Validators.required),
+      organizacion: new FormControl('', Validators.required),
+      fecha: new FormControl('', Validators.required),
+      comuna: new FormControl('', Validators.required),
+      ceroACinco: new FormControl('', [Validators.required, Validators.min(0)]),
+      seisADoce: new FormControl('', [Validators.required, Validators.min(0)]),
+      treceADiecisiete: new FormControl('', [Validators.required, Validators.min(0)]),
+      dieciochoAVentiocho: new FormControl('', [Validators.required, Validators.min(0)]),
+      ventiochoAMas: new FormControl('', [Validators.required, Validators.min(0)]),
+      descripcion: new FormControl('', Validators.required)
+    })
+  }
+
+  evento: FormGroup;
+
+  constructor() {
+    this.evento = this.createFormGroup();
+  }
 
   ngOnInit() {
+  }
+  onResetForm() {
+    this.evento.reset();
+  }
+  register() {
+    if (this.evento.valid) {
+      console.log(this.evento.value);
+    } else {
+      console.log("Campos mal validados");
+    }
+    this.onResetForm();
   }
 
 }
